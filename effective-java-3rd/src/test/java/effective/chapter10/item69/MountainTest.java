db63dfacf365370a32661efa5f1663b93d69443d
GYUTAEKIM <kkt219a@gmail.com>
2021-12-22 19:15:04 +0900

    [effective-java] 아이템 69 예외와 표준관용구 리스트

--- effective-java-3rd/src/test/java/effective/chapter10/item69/MountainTest.java
+++ effective-java-3rd/src/test/java/effective/chapter10/item69/MountainTest.java
@@ -2,30 +2,38 @@ package effective.chapter10.item69;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MountainTest {
-	private static final int MAX_ARRAY_SIZE = 10000000;
+	private static final int MAX_ARRAY_SIZE = 10_000_000;
 	private Mountain[] mountains;
+	private List<Mountain> mountainList;
 
 	@BeforeEach
 	public void init() {
 		mountains = new Mountain[MAX_ARRAY_SIZE];
+		mountainList = new ArrayList<>();
 		for(int i=0;i<MAX_ARRAY_SIZE;i++) {
 			mountains[i] = new Mountain();
+			mountainList.add(new Mountain());
 		}
 	}
 
 	@Test
-	public void testStandardIdiom() {
+	public void testStandardIdiomArrays() {
 		for (Mountain mountain : mountains) {
 			mountain.climb();
 		}
 	}
 
 	@Test
-	public void testUseException() {
+	public void testUseExceptionArrays() {
 		try {
 			int i=0;
 			while(true) {
@@ -36,5 +44,22 @@ class MountainTest {
 		}
 	}
 
+	@Test
+	public void testStandardIdiomList() {
+		for(Iterator<Mountain> i = mountainList.iterator(); i.hasNext(); ) {
+			Mountain mountain = i.next();
+		}
+	}
 
+	@Test
+	public void testUseExceptionList() {
+		try {
+			Iterator<Mountain> i = mountainList.iterator();
+			while(true) {
+				Mountain mountain = i.next();
+			}
+		} catch (NoSuchElementException e) {
+
+		}
+	}
 }
